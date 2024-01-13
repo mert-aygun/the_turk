0f5c96158879325c90bf5681aa54b50033eddf9a
Mert Aygun <aygunmert15@gmail.com>
2024-01-13 20:33:36 +0000

    yfinance stuff

--- test.py
+++ test.py
@@ -1,3 +1,25 @@
 #Testing Grounds
-import numpy as np
+import yfinance
+import requests_cache
+#First we need to understand what we're going to do.
+#One module of The Turk MUST include the ability to fetch ALL stock data.
 
+#We must have a tickers list that is sorted from A - Z
+#And must also have info beside such as sector (APPL, Technology) in order to easily query
+tickers = ["AAPL", "BTC"]
+
+#We must have a currency to compare the data with such as USD, GBP
+dovuz = "USD" # veya GBP
+
+session = requests_cache.CachedSession('yfinance.cache')
+session.headers['User-Agent'] = 'theturk'
+ticker = yfinance.Ticker("msft", session=session)
+
+
+txt = ""
+
+for key, value in ticker.info.items():
+    txt += f"{key} - {value}\n"
+
+with open("test.txt", "w") as file:
+    file.write(txt)
--- theturk/__init__.py
+++ theturk/__init__.py
@@ -2,6 +2,12 @@
 The Turk
 --------
 
+The Turk is a program that scrapes the internet's 
+vast financial information deposits in search for 
+assets that are appropriate for purchase based on
+certain trading specifications such as price, etc.
+
+- Mert Aygun, 13/01/2024, 00:21
 
 
 """
